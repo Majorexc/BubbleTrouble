43da7a4c9fb4e4bfb86e18ffca42cc474f483ef8
Teralitr <teralitr@ya.ru>
2018-06-25 19:35:24 +0300

    Implemented Timer, added bubble prefab and bubble material

---
+++ Assets/Scripts/Timer.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections;
+
+using UnityEngine;
+
+public class Timer : MonoBehaviour {
+    #region Static
+
+    public static event Action<float> TimeStarted;
+    public static event Action TimeEnded;
+    
+    #endregion
+
+    [SerializeField] float _gameDurationSeconds = 10;
+
+    Coroutine _timerCoroutine;
+    
+    void Start() {
+        StartTimer();
+    }
+
+    void StartTimer() {
+        Debug.Assert(_timerCoroutine != null, $"[{GetType()}] Timer is already working", this);
+        TimeStarted.Invoke(_gameDurationSeconds);
+        _timerCoroutine = StartCoroutine(TimerProgress(_gameDurationSeconds));
+    }
+
+    IEnumerator TimerProgress(float duration) {
+        yield return new WaitForSeconds(duration);
+        TimeEnded?.Invoke();
+        _timerCoroutine = null;
+    }
+    
+}
